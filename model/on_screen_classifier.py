35d3b45331f7838ff083bde154baf4237f15585c
ChasevdG <32102176+ChasevdG@users.noreply.github.com>
2020-12-08 11:51:53 -0600

    Update on_screen_classifier.py

--- model/on_screen_classifier.py
+++ model/on_screen_classifier.py
@@ -1,83 +1,35 @@
 import torch
 import torch.nn.functional as F
 
-def spatial_argmax(logit):
-    """
-    Compute the soft-argmax of a heatmap
-    :param logit: A tensor of size BS x H x W
-    :return: A tensor of size BS x 2 the soft-argmax in normalized coordinates (-1 .. 1)
-    """
-    weights = F.softmax(logit.view(logit.size(0), -1), dim=-1).view_as(logit)
-    return torch.stack(((weights.sum(1) * torch.linspace(-1, 1, logit.size(2)).to(logit.device)[None]).sum(1),
-                        (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
 
 class On_Screen_Classifier(torch.nn.Module):
-    class Block(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride)
-            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
-            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
-            self.b1 = torch.nn.BatchNorm2d(n_output)
-            self.b2 = torch.nn.BatchNorm2d(n_output)
-            self.b3 = torch.nn.BatchNorm2d(n_output)
-            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
-
-        def forward(self, x):
-            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
-
-    class UpBlock(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                               stride=stride, output_padding=1)
-
-        def forward(self, x):
-            return F.relu(self.c1(x))
-
-    def __init__(self, layers=[16, 32, 64, 128], n_class=1, kernel_size=3, use_skip=True):
-        
+    def __init__(self):
         super().__init__()
-        self.input_mean = torch.Tensor([0.3521554, 0.30068502, 0.28527516])
-        self.input_std = torch.Tensor([0.18182722, 0.18656468, 0.15938024])
 
-        c = 3
-        self.use_skip = use_skip
-        self.n_conv = len(layers)
-        skip_layer_size = [3] + layers[:-1]
-        for i, l in enumerate(layers):
-            self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
-            c = l
-        # Produce lower res output
-        for i, l in list(enumerate(layers))[::-1]:
-            self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
-            c = l
-            if self.use_skip:
-                c += skip_layer_size[i]
-        self.classifier = torch.nn.Conv2d(c, n_class, 1)
-        self.dist_classifier = torch.nn.Conv2d(c, n_class, 1)
-        self.size = torch.nn.Conv2d(c, 2, 1)
+        self.conv1 = torch.nn.Conv2d(3, 64, 5)
+        self.conv2 = torch.nn.Conv2d(64, 64, 5)
 
-    def forward(self, img):
-        """
-        Your code here
-        Predict the aim point in image coordinate, given the supertuxkart image
-        @img: (B,3,96,128)
-        return (B,2)
-        """
-        x = img
-        
-        z = (x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device)
-        up_activation = []
-        for i in range(self.n_conv):
-            # Add all the information required for skip connections
-            up_activation.append(z)
-            z = self._modules['conv%d' % i](z)
-                
-        out = self.classifier(z)
-        
-        return out
+        self.dropout = nn.Dropout()
+        self.pool = torch.nn.MaxPool2d(2, 2)
+
+        self.fc1 = torch.nn.Linear(64 * 13 * 13, 128)
+        self.fc2 = torch.nn.Linear(128, 32)
+        self.fc3 = torch.nn.Linear(32, 6)
+
+        self.relu = torch.nn.ReLU()
+        self.flatten = torch.nn.Flatten()
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool(x)
+        x = self.pool(self.conv2(x))
+
+        x = self.flatten(x)
+        x = self.relu(self.dropout(self.fc1(x)))
+        x = self.relu(self.fc3(self.dropout(self.relu(self.fc2(x)))))
+
+        return x
 
 
 def save_model(model):
